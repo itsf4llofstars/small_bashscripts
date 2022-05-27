5ba69a0f0a3124b1ce662e468246aeeca0b6798e
itsf4llofstars <irooted4hal@mailfence.com>
2022-05-27 00:17:08 -0500

    Setup inital check path script

---
+++ check-path.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#: TITLE:      check-path.sh
+#: DATE:       22/05/25
+#: AUTHOR:     GitHub: itsf4llofstars
+#: VERSION:    0.1
+#: DESCRPTION: A script to check if your path is good
+#: OPTIONS:    -h, --help
+
+#: Display help text
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    clear
+cat << EOF
+Running this with a user path setup either this script in the path directory
+or a link from the path directory to this script will check to ensure your
+path is set. Path is set if script runs successfully.
+
+Calls: For scripts in the path
+$ <script_name>.sh [options]
+
+options
+    -h, --help  This help text.
+EOF
+    exit 0
+fi
+
+#: START CODE HERE
+clear
+
+exit 0
