df535a7693a14ead0541c8482d9713232441ca26
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-02 22:11:04 -0500

    Add Description and call examples
    Removed man page default text

--- chmod.sh
+++ chmod.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#/usr/bin/env bash
 
 #: TITLE:      chmod.sh
 #: DATE:       22/05/26
@@ -11,13 +11,15 @@
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
     clear
 cat << EOF
-Description
+When called, changes the Linux permission of the file to 755.
+The same as calling:
+$ ./chmod 755 file
 
-Calls: If script not in the path
-$ ./chmod.sh [options]
+Calls: If script is not in the path
+$ ./chmod.sh [options] file
 
-Calls: If script in the path
-$ chmod.sh [options]
+Calls: If script is n the path
+$ chmod.sh [options] file
 
 options
     -h, --help  This help text.
@@ -25,8 +27,6 @@ EOF
     exit 0
 fi
 
-#: Run if no -h or --help option passed as command line argument.
-#: START CODE HERE
 if [ ! -z "$1" ]; then
     chmod 755 "$1"
 else
@@ -35,45 +35,5 @@ else
     exit 1
 fi
 
-# exit 0 is likely not needed.
 exit 0
-#: END CODE HERE
 
-#: Documentation for man page creation.
-#: Create man page by:
-#: Filling in the sections below with your code and text. Don't touch the lines with the equals
-#: sign. Run the command below this line in the command prompt by replacing the text between the
-#: carots, along with the carots, with the scritps name. This is still a work in progress.
-
-#: $ pod2man <script_name>.sh > <script_name>[.ext]
-
-: <<'END_OF_DOCS'
-=head1 NAME
-
-    NAME <script_name.sh> [option]
-
-=head1 SYNOPSIS
-
-    SYNOPSIS
-
-=head1 DESCRIPTION
-
-    DESCRIPTION
-
-=head1 EXAMPLES
-
-    EXAMPLE calls
-
-=head1 OVERVIEW
-
-    OVERVIEW
-
-=head1 DEFAULTS
-
-    DEFAULTS
-
-=head1 OPTIONS
-
-    OPTIONS
-
-END_OF_DOCS
